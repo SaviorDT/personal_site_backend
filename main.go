def14f7d657c7c1868fa14a6ef13eb88cedab29f
deeelol <tony200404242@gmail.com>
2025-07-11 18:46:00 +0000

    Database model.

--- main.go
+++ main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
+	if err := InitDB(); err != nil {
+		panic(err)
+	}
+
 	r := gin.Default()
 	r.GET("/hello", func(c *gin.Context) {
-		c.String(200, "Hello, World!")
+		db, err := DB.DB()
+		if err != nil {
+			c.String(http.StatusInternalServerError, "DB error: %v", err)
+			return
+		}
+		err = db.Ping()
+		if err != nil {
+			c.String(http.StatusInternalServerError, "DB ping failed: %v", err)
+			return
+		}
+		c.String(200, "DB connection OK!")
 	})
 	r.Run(":80") // Start the server on port 8080
 }

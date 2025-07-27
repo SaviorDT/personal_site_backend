14a38ca895ff8e5b3adb4f21fa6ee747a56df66e
deeelol <tony200404242@gmail.com>
2025-07-27 18:45:50 +0000

    cors

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"strings"
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 
 	"personal_site/config"
@@ -20,6 +24,24 @@ func main() {
 	}
 
 	r := gin.Default()
+
+	// CORS 配置
+	allowedOrigins, _ := config.GetVariableAsString("CORS_ALLOWED_ORIGINS")
+
+	corsConfig := cors.Config{
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"*"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}
+
+	if allowedOrigins != "" {
+		corsConfig.AllowOrigins = strings.Split(allowedOrigins, ",")
+	}
+
+	r.Use(cors.New(corsConfig))
+
 	routers.RegisterRouters(r, db)
 
 	r.Run(":80") // Start the server on port 8080

d350b6096771d264438c05d438fdafa85befbfa1
deeelol <tony200404242@gmail.com>
2026-05-21 14:57:10 +0000

    Update(battle cat): Show the time least used.

--- controllers/battle_cat/battle_cat.go
+++ controllers/battle_cat/battle_cat.go
@@ -1,7 +1,9 @@
 package battlecat
 
 import (
+	"fmt"
 	"personal_site/models"
+	"sort"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -15,6 +17,7 @@ type FilterLevelsRequest struct {
 
 type levelData struct {
 	Level   string `json:"level"`
+	Time	string `json:"time"`
 	Name    string `json:"name"`
 	HP      uint16 `json:"hp"`
 	Enemies string `json:"enemies"`
@@ -44,6 +47,12 @@ func FilterLevels(c *gin.Context, db *gorm.DB) {
 	}
 	permuteFindLevels1(req.Enemies, &collections, allPossibleLevels)
 
+	for i := range collections {
+		sort.Slice(collections[i].Levels, func(a, b int) bool {
+			return !LongerThan(collections[i].Levels[a].Time, collections[i].Levels[b].Time)
+		})
+	}
+
 	c.JSON(200, collections)
 }
 
@@ -54,7 +63,7 @@ func permuteFindLevels1(enemies []string, collections *[]levelCollection, allPos
 
 		for _, level := range allPossibleLevels {
 			if containsEnemy(level.Enemies, enemy) {
-				collection.Levels = append(collection.Levels, levelData{Level: level.Level, Name: level.Name, HP: level.HP, Enemies: level.Enemies})
+				collection.Levels = append(collection.Levels, levelData{Level: level.Level, Time: calMaxTime(level.Enemies, []string{enemy}), Name: level.Name, HP: level.HP, Enemies: level.Enemies})
 			}
 		}
 		*collections = append(*collections, collection)
@@ -67,9 +76,10 @@ func permuteFindLevels2(enemies []string, collections *[]levelCollection, allPos
 			var collection levelCollection
 			collection.Enemies = []string{enemy1, enemy2}
 
-			for _, level := range allPossibleLevels {
+			for i, level := range allPossibleLevels {
 				if containsEnemy(level.Enemies, enemy1) && containsEnemy(level.Enemies, enemy2) {
-					collection.Levels = append(collection.Levels, levelData{Level: level.Level, Name: level.Name, HP: level.HP, Enemies: level.Enemies})
+					allPossibleLevels[i].Enemies = ""
+					collection.Levels = append(collection.Levels, levelData{Level: level.Level, Time: calMaxTime(level.Enemies, []string{enemy1, enemy2}), Name: level.Name, HP: level.HP, Enemies: level.Enemies})
 				}
 			}
 			*collections = append(*collections, collection)
@@ -81,9 +91,10 @@ func permuteFindLevels3(enemies []string, collections *[]levelCollection, allPos
 	var collection levelCollection
 	collection.Enemies = enemies
 
-	for _, level := range allPossibleLevels {
+	for i, level := range allPossibleLevels {
 		if containsEnemy(level.Enemies, enemies[0]) && containsEnemy(level.Enemies, enemies[1]) && containsEnemy(level.Enemies, enemies[2]) {
-			collection.Levels = append(collection.Levels, levelData{Level: level.Level, Name: level.Name, HP: level.HP, Enemies: level.Enemies})
+			allPossibleLevels[i].Enemies = ""
+			collection.Levels = append(collection.Levels, levelData{Level: level.Level, Time: calMaxTime(level.Enemies, enemies), Name: level.Name, HP: level.HP, Enemies: level.Enemies})
 		}
 	}
 	*collections = append(*collections, collection)
@@ -92,9 +103,71 @@ func permuteFindLevels3(enemies []string, collections *[]levelCollection, allPos
 func containsEnemy(enemies string, target string) bool {
 	enemyList := strings.Split(enemies, "、")
 	for _, enemy := range enemyList {
+		if idx := strings.Index(enemy, "（"); idx != -1 {
+			enemy = enemy[:idx]
+		}
+		enemy = strings.TrimSpace(enemy)
 		if enemy == target {
 			return true
 		}
 	}
 	return false
+}
+
+func calMaxTime(levelEnemies string, targetEnemies []string) string {
+	var result string = "101%"
+
+	enemyList := strings.Split(levelEnemies, "、")
+	for _, enemy := range enemyList {
+		var time string = "101%"
+		if idx := strings.Index(enemy, "（"); idx != -1 {
+			original := enemy
+			enemy = enemy[:idx]
+			time = original[idx+3 : len(original)-3]
+		}
+		enemy = strings.TrimSpace(enemy)
+		for _, target := range targetEnemies {
+			if enemy == target {
+				result = longerTime(result, time)
+				break
+			}
+		}
+	}
+	return result
+}
+
+func longerTime(time1 string, time2 string) string {
+	if LongerThan(time1, time2) {
+		return time1
+	}
+	return time2
+}
+
+func LongerThan(time1 string, time2 string) bool {
+	var val1, val2 int
+	var format1, format2 byte
+	fmt.Sscanf(time1[:len(time1)-1], "%d", &val1)
+	fmt.Sscanf(time2[:len(time2)-1], "%d", &val2)
+	format1 = time1[len(time1)-1]
+	format2 = time2[len(time2)-1]
+
+	if format1 != format2 {
+		if format1 == 's' {
+			return true
+		}
+		return false
+	}
+
+	if format1 == 's' {
+		if val1 > val2 {
+			return true
+		}
+		return false
+	}
+
+	// 10% longer than 20%
+	if val1 > val2 {
+		return false
+	}
+	return true
 }
--- models/battle_cat_level.go
+++ models/battle_cat_level.go
@@ -6,5 +6,5 @@ type BattleCatLevel struct {
 	Level   string `gorm:"size:16;not null;Index"`
 	Name    string `gorm:"size:128;not null;Index"`
 	HP      uint16 `gorm:"not null"`
-	Enemies string `gorm:"size:64;not null;Index"`
+	Enemies string `gorm:"size:256;not null"`
 }
